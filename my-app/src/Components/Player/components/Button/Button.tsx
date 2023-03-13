5067ff07fbf7099f2853c237d5bcd39f2ea4638b
nickandreev <nandreev294@gmail.com>
2023-03-13 19:37:04 +0300

    button changed

--- my-app/src/Components/Player/components/Button/Button.tsx
+++ my-app/src/Components/Player/components/Button/Button.tsx
@@ -1,27 +1,27 @@
-import React, {FC} from 'react'
-import cn from 'classnames';
+import React, { FC } from 'react'
+import cn from 'classnames'
 
 import s from './Button.module.scss'
 
 interface IButtonProps {
-    onClick: () => void
+  onClick: () => void
 }
 
-const Button:FC<IButtonProps> = ({ onClick }) => {
-    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-        onClick();
-        e.currentTarget.classList.toggle(s.active)
-    }
-    return (
-        <div className={s.botón} onClick={handleClick}>
-            <div className={s.fondo}></div>
-            <div className={s.icono}>
-                <div className={cn(s.parte, s.izquierda)}></div>
-                <div className={cn(s.parte, s.derecha)}></div>
-            </div>
-            <div className={s.puntero}></div>
-        </div>
-    )
+const Button: FC<IButtonProps> = ({ onClick }) => {
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    onClick()
+    e.currentTarget.classList.toggle(s.active)
+  }
+  return (
+    <div className={s.button} onClick={handleClick}>
+      <div className={s.back}></div>
+      <div className={s.icon}>
+        <div className={cn(s.part, s.left)}></div>
+        <div className={cn(s.part, s.right)}></div>
+      </div>
+      <div className={s.pointer}></div>
+    </div>
+  )
 }
 
 export default Button
